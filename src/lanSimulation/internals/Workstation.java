4ff46d001b2e87a6e7965c8f0cdb2ba5283f79e9
Edgardo Zelada <ezs0003@alu.ubu.es>
2015-06-04 17:59:17 +0200

    #4 Creadas subclases vacias para los distintos tipos de nodo

---
+++ src/lanSimulation/internals/Workstation.java
@@ -0,0 +1,15 @@
+package lanSimulation.internals;
+
+public class Workstation extends Node {
+
+	public Workstation(byte type, String name) {
+		super(type, name);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Workstation(byte type, String name, Node nextNode) {
+		super(type, name, nextNode);
+		// TODO Auto-generated constructor stub
+	}
+
+}
